bfd7641ba278dd05eed7654ed7b57e8402e258b1
Rakib <rakib@Rakibs-MacBook-Pro.local>
2025-08-11 00:18:09 +0600

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavbarMain from './components/navbar/NavbarMain'
+
+export default function App() {
+  return (
+   <main className='font-body'>
+    <NavbarMain />
+   </main>
+  )
+}
---
+++ src/components/navbar/NavbarLogo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NavbarLogo() {
+  return (
+    <div>
+      <h1 className ="text-2xl text-white">Rakib Ul Islam</h1>
+    </div>
+  )
+}
